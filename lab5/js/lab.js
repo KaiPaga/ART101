e91d315206b3bfdb2f665c5c498c49a8a48c089d
KaiPaga <kkpagan@ucsc.edu>
2024-04-28 13:23:53 -0700

    Lab 6: Arrays and Objects

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,25 +1,32 @@
 /*
 * Aruthor: Kai Pagan <kkpagan@ucsc.edu>
-* Created: 25 April
+* Created: 27 April
 * License: Public Domain
 */
 
 
-// Define variables 
-make = "Santa Cruz Metro Buses";
-model = "Route 18";
-color = "White with Black Windows";
-year = "2023";
+// Define Variables
+myTransport = ["lyft and uber services", "public buses","the bart and other trian systems", "walking"];
 
-// calculate
-age = 2023 - year;
+
+//create an object for my main ride 
+myMainRide = {
+    make = "Santa Cruz Metro Buses";
+    model = "Route 18";
+    color = "White with Black Windows";
+    year = "2023";
+    // we can define a function within our obejct (called a method)
+    // that uses the value of the year above (using this.year)
+    age : function() {
+        return 2025 - this.year;
+    }
+  }
 
 // output 
-document.writeln("Make: " + make + "<br>") ; 
-document.writeln("Model: " + model + "<br>") ;
-document.writeln("Color: " + color + "<br>") ;
-document.writeln("Year: " + year + "<br>") ;
-document.writeln("Age: " + age + " years<br>") ;
+document.writeln("Kinds of transport I use: ", myTransport, "<br>") ; 
+document.writeln("My Main Ride: <pre>",
+     JSON.stringify(myMainRide, null, '/t'), "</pre>");
+ ;
 
 // Functions
 
--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+* Aruthor: Kai Pagan <kkpagan@ucsc.edu>
+* Created: 27 April
+* License: Public Domain
+*/
 
-// Constants
 
-// Functions
+// Define Variables
+myTransport = ["lyft and uber services", "public buses","the bart and other trian systems", "walking"];
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//create an object for my main ride 
+myMainRide = {
+    make: "Santa Cruz Metro Buses",
+    model: "Route 18",
+    color: "White with Black Windows",
+    year: 2023,
+    // we can define a function within our obejct (called a method)
+    // that uses the value of the year above (using this.year)
+    age : function() {
+        return 2025 - this.year;
+    }
+  }
 
-// let's get this party started
-main();
+// output 
+document.writeln("Kinds of transportion I use: ", myTransport, "<br>") ; 
+document.writeln("My Main Ride: <pre>",
+     JSON.stringify(myMainRide, null, '\t'), "</pre>");
+ 
